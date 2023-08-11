4446dad4b379b54dea93f7055d0ad0d9657f5c9f
Timothy <timsu100@gmail.com>
2023-08-11 12:44:15 -0700

    add pet and login route

--- controller/home-route.js
+++ controller/home-route.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
-const { Pet } = require('../models');
+const { User, Pet } = require('../models');
 
+//Homeroute
+router.get('/', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/pets')
+    return;
+  } else {
+    res.redirect('/login');
+    return;
+  }
+});
+
+//User pets
 router.get('/pets', async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.id, {
@@ -26,3 +38,31 @@ router.get('/pets', async (req, res) => {
     res.status(500).json(err)
   }
 });
+
+//Specific Pet
+router.get('/pets/:id', async (req, res) => {
+  try {
+    const petData = await Pet.findByPk(req.params.id)
+
+    const pet = petData.get({ plain: true });
+    res.render('placeholder-for-specific-pet', {
+      ...pet,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+//login
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/pets')
+    return;
+  }
+  res.render('login');
+
+});
+
+
+module.exports = router;
