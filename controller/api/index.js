b1a2c7b181693383c7b83daba4e7fdf7e0eba295
Timothy <timsu100@gmail.com>
2023-08-13 23:00:56 -0700

    add interact route

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const petRoutes = require('./petRoutes');
+const interactRoutes = require('./interactRoutes')
 
 router.use('/users', userRoutes);
 router.use('/pets', petRoutes);
+router.use('/')
+
 
 module.exports = router;
